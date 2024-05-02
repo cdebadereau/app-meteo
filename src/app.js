9584ce9eccffb6a08d79f20cbde5912dd0132c30
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2024-05-02 16:47:40 +0200

    Made the api call for the search engine

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
-function searchCity(event) {
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector(
+    "#weather-app-temperature-value"
+  );
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let cityName = document.querySelector("#weather-app-city");
+  cityName.innerHTML = response.data.city;
+  temperatureElement.innerHTML = currentTemperature;
+}
+
+function searchCity(city) {
+  let apiKey = "96668c3bo8171924ab023b1atd05c04f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityName = document.querySelector("#weather-app-city");
-  cityName.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Montréal");
